1dcaf200377370ccd8eafb7bb5d0e771aa7903f1
Zyborip <108153243+CyberHipp@users.noreply.github.com>
2023-07-26 03:22:01 +0200

    Update ATOM_Framework.py

--- src/ATOM_Framework.py
+++ src/ATOM_Framework.py
@@ -1,14 +1,30 @@
-import tensorflow as tf
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Dense
-
-class AIModel:
+class ATOMFramework:
     def __init__(self, model):
-        self.model = model
+        self.ai_model = AIModel(model)
+        self.memory_enhancer = MemoryEnhancer()
+        self.output_controller = OutputController()
+        self.theory_of_mind_enhancer = TheoryOfMindEnhancer()
+        self.thought_chain = ThoughtChain()
+        self.experts = self._initialize_experts()
+        self.agent = Agent(self.experts)
 
-    def generate_response(self, prompt):
-        # Generate a response given a prompt
-        # This function will need to be fleshed out with the specifics of how the AI model generates a response.
-        # This could involve pre-processing the prompt, feeding the prompt to the model, post-processing the model's output, etc.
-        # Since this is highly dependent on the specifics of the AI model being used, it's left as a placeholder here.
-        return self.model.generate(prompt)
+    def _initialize_experts(self):
+        # Initialize expert models (this could be instances of specific AI models)
+        expert_1 = Expert('expert_1', AIModel(None))
+        expert_2 = Expert('expert_2', AIModel(None))
+        return [expert_1, expert_2]
+
+    def process_input(self, user_input):
+        self.thought_chain.add_thought(user_input)
+
+        last_n_thoughts = self.thought_chain.get_last_n_thoughts(5) # Get last 5 thoughts, this number can be adjusted
+        prompt = self.memory_enhancer.construct_prompt(last_n_thoughts) # Use memory enhancer to construct the prompt
+
+        raw_response = self.ai_model.generate_response(prompt)
+        
+        controlled_response = self.output_controller.control_output(raw_response) # Control the output of the response
+        enhanced_response = self.theory_of_mind_enhancer.enhance_response(controlled_response) # Enhance the response with theory of mind
+
+        self.thought_chain.add_thought(enhanced_response)
+
+        return enhanced_response
