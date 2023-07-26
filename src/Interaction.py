4315ba886a3f67ba5f6a67b4f714c9e3f47c62a0
Zyborip <108153243+CyberHipp@users.noreply.github.com>
2023-07-26 02:15:10 +0200

    Create Interaction.py

---
+++ src/Interaction.py
@@ -0,0 +1,5 @@
+class Interaction:
+    def __init__(self, prompt, response, expert_specialty):
+        self.prompt = prompt
+        self.response = response
+        self.expert_specialty = expert_specialty
