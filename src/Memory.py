f1ddd4fb2ed17ff6492a52a9b7d9a53e88dee2eb
Zyborip <108153243+CyberHipp@users.noreply.github.com>
2023-07-26 03:27:06 +0200

    Update Memory.py

--- src/Memory.py
+++ src/Memory.py
@@ -1,20 +1,37 @@
 # Memory.py
+# Memory.py
+
+from collections import deque
+from sentence_transformers import SentenceTransformer
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
 
 class Memory:
-
     def __init__(self):
         # Initialize an empty memory
         self.records = []
+
+    def __init__(self, max_size=1000):
+        # Initialize an empty memory
+        self.records = deque(maxlen=max_size)
+        self.embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
 
     def remember(self, interaction):
         # Store the interaction in memory
         self.records.append(interaction)
 
-    def get_last_n(self, n):
-        # Get the last n records from memory
-        return self.records[-n:]
-
-    def recall(self, context):
+    def recall(self, context, top_k=1):
         # Retrieve relevant memory given a context
-        # This could involve complex logic like searching for similar contexts
-        pass
+        # Convert context and records to embeddings
+        context_embedding = self.embedding_model.encode([context])
+        record_embeddings = self.embedding_model.encode([record.prompt for record in self.records])
+        # Compute cosine similarity
+        similarities = cosine_similarity(context_embedding, record_embeddings)
+        # Get top k indices
+        top_indices = np.argsort(similarities, axis=-1)[:, -top_k:]
+        # Return top k records
+        return [self.records[index] for index in top_indices[0]]
+
+    def get_last_n(self, n):
+        # Get the last n interactions from memory
+        return list(self.records)[-n:]
