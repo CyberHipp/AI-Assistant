d2b9207bded76c01d51fed4b8a85b0043b57f893
Zyborip <108153243+CyberHipp@users.noreply.github.com>
2023-07-26 02:47:38 +0200

    Update Memory_Enhancer.py

--- src/Memory_Enhancer.py
+++ src/Memory_Enhancer.py
@@ -1,4 +1,7 @@
+# MemoryEnhancer.py
+
 class MemoryEnhancer:
+
     def __init__(self):
         self.model = None # Some model
 
@@ -8,3 +11,18 @@ class MemoryEnhancer:
         # This could involve concatenating the thoughts, adding specific formatting or keywords, etc.
         # Since this is highly dependent on the specifics of the AI model and the memory enhancer being used, it's left as a placeholder here.
         return self.model.construct_prompt(last_n_thoughts)
+
+    def __init__(self, memory):
+        self.memory = memory
+
+    def construct_prompt(self, n):
+        # Get the last n interactions from memory
+        last_n_interactions = self.memory.get_last_n(n)
+
+        # Extract the textual prompts
+        last_n_prompts = [interaction.prompt for interaction in last_n_interactions]
+
+        # Join the prompts into a single context string 
+        prompt_context = " ".join(last_n_prompts)
+
+        return prompt_context
