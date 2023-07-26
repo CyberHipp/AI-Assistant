3ca96803a4177a72d010e13a06cb03a95e84e72e
Zyborip <108153243+CyberHipp@users.noreply.github.com>
2023-07-26 12:19:48 +0200

    Update Theory_of_Mind_Enhancer.py

--- src/Theory_of_Mind_Enhancer.py
+++ src/Theory_of_Mind_Enhancer.py
@@ -1,9 +1,17 @@
 class TheoryOfMindEnhancer:
-    def __init__(self):
-        self.model = None # Some model
+    def __init__(self, model, memory):
+        self.model = model  # Some model
+        self.memory = memory  # Use Memory instance to access the conversation history
 
     def enhance_response(self, controlled_response):
         # Enhance the response with theory of mind
-        # This function will need to be fleshed out with the specifics of how the response is enhanced.
-        # Since this is highly dependent on the specifics of the AI model and the Theory of Mind enhancer being used, it's left as a placeholder here.
-        return self.model.enhance_response(controlled_response)
+        # For now, let's just make the assistant refer back to the previous user input in its response
+        last_user_input = self.memory.get_last_n(1)[0].prompt  # Get the last user input from memory
+
+        # Use the model to enhance the response
+        enhanced_response = self.model.enhance_response(controlled_response)
+
+        # Append a reference to the last user input to the assistant's response
+        enhanced_response = f"In response to what you said about '{last_user_input}', {enhanced_response}"
+
+        return enhanced_response
