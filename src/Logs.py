a8ccdbdc27bcee75cf590682ebbbafeb8608f55a
Zyborip <108153243+CyberHipp@users.noreply.github.com>
2023-07-26 13:20:22 +0200

    Update Logs.py

--- src/Logs.py
+++ src/Logs.py
@@ -1,61 +1,35 @@
 import logging
-import os
 import json
+from logging.handlers import RotatingFileHandler
 
 class CustomFormatter(logging.Formatter):
-    """Custom formatter, overrides funcName with value of funcname if it exists"""
+    """Custom formatter to output logs in JSON with additional metadata"""
+
     def format(self, record):
-        if hasattr(record, 'funcname'):
-            record.funcName = record.funcname
-        return super().format(record)
+        log_record = {
+            "timestamp": self.formatTime(record, self.datefmt),
+            "level": record.levelname,
+            "message": record.getMessage(),
+            "file": record.filename,
+            "line": record.lineno,
+            "function": record.funcName
+        }
+        if record.exc_info:  # Capture exception info if available
+            log_record["exc_info"] = self.formatException(record.exc_info)
+        return json.dumps(log_record)
 
-class JsonLogger:
-    """Logger that outputs log messages in JSON format."""
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
-    def __init__(self, name, log_file=None):
-        """Create an instance of JsonLogger.
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_formatter = CustomFormatter()
+console_handler.setFormatter(console_formatter)
+logger.addHandler(console_handler)
 
-        If log_file is specified, log output will be written to that file,
-        otherwise log output will go to the console.
-        """
-        self.logger = logging.getLogger(name)
-        self.logger.setLevel(logging.DEBUG)
-
-        formatter = CustomFormatter(
-            '{"timestamp": "%(asctime)s", "level": "%(levelname)s", "message": %(message)s,'
-            ' "file": "%(filename)s", "funcName": "%(funcName)s", "lineno": %(lineno)d}\n',
-            "%Y-%m-%d %H:%M:%S")
-        if log_file:
-            file_handler = logging.FileHandler(log_file)
-            file_handler.setFormatter(formatter)
-            self.logger.addHandler(file_handler)
-        else:
-            console_handler = logging.StreamHandler()
-            console_handler.setFormatter(formatter)
-            self.logger.addHandler(console_handler)
-
-    def _log(self, level, msg, **kwargs):
-        """Log a message at a specified level.
-
-        The message can include placeholders for variable data, which are
-        provided as keyword arguments.
-        """
-        if kwargs:
-            msg += ": " + ', '.join(f'{k}={v}' for k, v in kwargs.items())
-        self.logger.log(level, msg)
-
-    def debug(self, msg, **kwargs):
-        self._log(logging.DEBUG, msg, **kwargs)
-
-    def info(self, msg, **kwargs):
-        self._log(logging.INFO, msg, **kwargs)
-
-    def warning(self, msg, **kwargs):
-        self._log(logging.WARNING, msg, **kwargs)
-
-    def error(self, msg, **kwargs):
-        self._log(logging.ERROR, msg, **kwargs)
-
-    def exception(self, msg, **kwargs):
-        self._log(logging.ERROR, msg, **kwargs)
-        self.logger.exception(msg)
+# Rotating File Handler (max 5 files of 2MB each)
+file_handler = RotatingFileHandler('app.log', maxBytes=2*1024*1024, backupCount=5)
+file_handler.setLevel(logging.DEBUG)
+file_formatter = CustomFormatter()
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
